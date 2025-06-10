c7a59d579c38d2fcec802a2e88e748e01ddeff18
B. Scott Michel <scooter.phd@gmail.com>
2025-06-10 09:41:30 -0700

    doc updates

--- sim_tailq.h
+++ sim_tailq.h
@@ -2,7 +2,7 @@
  * sim_tailq_t: Lock-free (mostly) tail queue, where inserting elements at the head of the queue and appending elements
  * at the tail are atomically compared/exchanged.
  *
- * Lock-free (mostly): As with atomic variables above, use (in order of precedence and availability):
+ * Lock-free (mostly): In order of precedence and availability:
  *  -- C11 and newer standard atomic operations
  *  -- Compiler intrinsics (GCC, Clang and MSVC)
  *  -- Mutex guard as a last resort
@@ -13,25 +13,30 @@
 #include "support/sim_bool.h"
 #include "sim_atomic.h"
 
-/*!
- * \brief Tail queue item type.
- * 
- * Generic pointer to items stored in the tail queue.
- */
-typedef struct {} *sim_tailq_item_t;
-
-/*!
- * \brief Tail queue item status
+/*! \brief Tail queue item type.
  *
- * Item status indicator while modifying the item pointer.
+ * Generic pointer to items stored in the tail queue. This forces type casts to
+ * the desired pointer type and avoid the potential bugs that can arise from
+ * `void *`.
+ */
+typedef struct sim_tailq_item_s {
+    struct sim_tailq_item_s *__dummy_ignored_never_used;
+} *sim_tailq_item_t;
+
+/*! \brief Item status indicator.
+ *
+ * An internal `item` status indicator that ensures that the `sim_tailq_elem_t->item`
+ * member is satble. `TAILQ_ITEM_READY` indicates that the `item` pointer is stable,
+ * i.e., not being modified. `TAILQ_ITEM_BUSY` indicates that the `item` pointer is
+ * not stable, i.e., being modified, and a dequeue operation needs to wait until
+ * the status changes to `TAILQ_ITEM_READY`.
  */
 typedef enum sim_tailq_item_status_e {
   TAILQ_ITEM_READY,
   TAILQ_ITEM_BUSY
 } sim_tailq_item_status_t;
 
-/*!
- * Tail queue list element type.
+/*! \brief * Tail queue list element type.
  */
 typedef struct sim_tailq_elem_s {
     /*! Generic element pointer. */
@@ -42,7 +47,22 @@ typedef struct sim_tailq_elem_s {
     SIM_ATOMIC_TYPE(struct sim_tailq_elem_s *) next;
 } sim_tailq_elem_t;
 
-/* The list type, itself. */
+/* \brief The tailq queue list type.
+ *
+ * An atomic access controlled queue that also keeps track of the
+ * number of elements in the queue and the number of elements
+ * allocated.
+ * 
+ * The queue is initialized using `sim_tailq_init()` or with a
+ * paired mutex via `sim_tailq_paired_init()` when mutexes are
+ * the sole synchronization primitive.
+ * 
+ * Items are enqueued and dequeued using `sim_tailq_enqueue()`
+ * and `sim_tailq_dequeue()`.
+ * 
+ * `sim_tailq_destroy()` * cleans the queue when it is no longer
+ * needed.
+ */
 typedef struct sim_tailq_s {
     /*! List head, first element. */
     SIM_ATOMIC_TYPE(sim_tailq_elem_t *) head;
@@ -53,7 +73,7 @@ typedef struct sim_tailq_s {
     /*! Maximum number of elements allocated. */
     sim_atomic_value_t n_allocated;
 
-    #if NEED_VALUE_MUTEX
+#if NEED_VALUE_MUTEX
     /*!
      * \brief Modification mutex
      *
@@ -76,22 +96,25 @@ typedef struct sim_tailq_s {
 #endif
 } sim_tailq_t;
 
-/* Transformation function type. */
+/*! \brief Enqueue transformation function type.
+ *
+ * `sim_tailq_enqueue_xform()` takes a function whose prototype is
+ * `sim_tailq_xform_t` to transform an existing item into a new item.
+ * Note that the existing item may be a `NULL` pointer.
+ */
 typedef sim_tailq_item_t (*sim_tailq_xform_t)(sim_tailq_item_t, void *);
 
-/*!
- * \brief Initialize an atomic tail queue.
+/*! \brief Initialize an atomic tail queue.
  *
- * Bulk allocates an initial tail queue list, makes the list circular, sets the head, tail and
- * last element pointers.
+ * Bulk allocates an initial tail queue list, makes the list circular,
+ * sets the head and tail element pointers.
  * 
  * \param tailq The tail queue to be initialized.
  * \return 0 on error, otherwise 1.
  */
 int sim_tailq_init(sim_tailq_t *tailq);
 
-/*!
- * \brief Initialize an atomic tail queue paired with a mutex.
+/*! \brief Initialize an atomic tail queue paired with a mutex.
  *
  * 
  * 
